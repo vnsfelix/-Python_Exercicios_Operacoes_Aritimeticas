227f6004f9bf6bda2398c00355babcfe17633a06
Vinicius Felix <vfelix2022santos@gmail.com>
2026-05-20 11:13:05 -0300

    correção ex10

--- ex10.py
+++ ex10.py
@@ -2,6 +2,6 @@
 # Mostre o resultado da divisão de -7 por 2
 # usando divisão inteira e o operador de resto.
 num = -7
-divisao = int(-7 / 2)
+divisao = (-7 // 2)
 resto = -7 % 2
 print(f"A divisão inteira de -7 por 2 é: {divisao}, com o resto: {resto}")
